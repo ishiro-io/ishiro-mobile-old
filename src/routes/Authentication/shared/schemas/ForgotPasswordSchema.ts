41696d5475091718fac8cc6a0582744878fc2736
MaelissCoue <maeliss.lrcoue@gmail.com>
2020-12-01 14:00:45 +0100

    ✨Add Regex phoneNumber (ForgotPassword)

--- src/routes/Authentication/shared/schemas/ForgotPasswordSchema.ts
+++ src/routes/Authentication/shared/schemas/ForgotPasswordSchema.ts
@@ -1,7 +1,12 @@
 import * as Yup from "yup";
 
 const ForgotPasswordSchema = Yup.object().shape({
-  phoneNumber: Yup.string().required("Votre numéro de téléphone  est requis")
+  phoneNumber: Yup.string()
+    .matches(
+      /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s.-]*\d{2}){4}$/,
+      "Le numéro est incorrect"
+    )
+    .required("Votre numéro de téléphone est requis")
 });
 
 export default ForgotPasswordSchema;
